523117d438f0dabe7eeae25c29589f9d50877626
= <=>
2024-03-16 13:20:59 +0700

    second commit

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -7,7 +7,7 @@ const HomePage = () => {
       <div className=" w-full absolute -translate-y-1/2 flex justify-center  top-1/2">
         <RoutingPage />
       </div>
-      {/* <Canvas /> */}
+      <Canvas />
     </div>
   );
 };
